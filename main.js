adeb2d7722feb0ef021c9ad1323a4fa91dd93582
GianFire <gian.delprado94@gmail.com>
2019-04-06 11:27:56 -0700

    completed project

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 // write your code here
-
+$.getJSON( "data.json", function( data ) {
+      var tableData = [];
+    $.each( data, function( key, val ) {
+        tableData.push('<tr><td>' + this.name + '</td>' + '<td>' + this.description + '</td>' + '<td>' + '<a href=https://www.google.com/maps?q='+ this.location + ' id="button"button>Link</a></td></tr>');
+    });
+   
+    $( "<tbody/>", {
+      "name": "",
+      "description": "",
+      "location": "",
+      html: tableData.join( "" )
+    }).appendTo( "tbody" );
+});
 });
